47b455fae88a8dbdfc9eccd021f4cc3147e5e9a3
jkrone04 <45321053+jkrone04@users.noreply.github.com>
2018-11-24 18:02:22 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,10 +1,3 @@
-/*
- * Filename: LinkedList.h
- * Contains: Interface of the template LinkedList class 
- * Part of: Homework 5 assignment "Graph Traversal" for Comp 15 Fall 2018
- *
- */
-
 #include <iostream>
 #include <exception>
 #include "NodeType.h"
@@ -15,10 +8,6 @@ using namespace std;
 
 const int FAIL = -1;
 
-// A good way to handle errors is with exceptions
-// This class has a couple of them for common errors
-// In this case we have one when there is no memory to 
-// allocate and when someone does an invalid operation 
 class FullList : public exception{
 public:
     const char * what() const throw() {return "Oh no, the list is full!\n";}; 
